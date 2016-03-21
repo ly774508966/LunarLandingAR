86c26120a702620ae76d90fa3cf0971e54c87fec
wkoeter <willemkoeter@outlook.com>
2016-03-21 14:03:29 +0100

    Left&right movement

--- LunarLanding/Assets/Scripts/PlayerController.cs
+++ LunarLanding/Assets/Scripts/PlayerController.cs
@@ -5,9 +5,11 @@ public class PlayerController : MonoBehaviour {
 
     Rigidbody _rigidbody;
     public Vector3 vector = new Vector3(0,2000f,0);
+    public Vector3 vector2 = new Vector3(2000f, 0, 0);
+    public Vector3 vector3 = new Vector3(-2000f, 0, 0);
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
         _rigidbody = GetComponent<Rigidbody>();
 	}
@@ -15,11 +17,19 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        if(Input.GetAxis("Vertical")>.1f)
+        if(Input.GetAxis("Vertical")>0)
         {
             _rigidbody.AddForce(vector * Time.deltaTime);
         }
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            _rigidbody.AddForce(vector3 * Time.deltaTime);
+        }
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            _rigidbody.AddForce(vector2 * Time.deltaTime);
+        }
         // zorgen dat het per seconde gaat idpv per frame
-        
-	}
+
+    }
 }
