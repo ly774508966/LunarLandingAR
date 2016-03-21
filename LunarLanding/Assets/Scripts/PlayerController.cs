11e7a744126685bb2759162f5c37662f0875c972
Koeter <willem.koeter.external@atos.net>
2016-03-21 12:05:28 +0100

    Update1 met playercontroller

---
+++ LunarLanding/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody _rigidbody;
+    public Vector3 vector = new Vector3(0,2000f,0);
+
+	// Use this for initialization
+	void Start ()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Input.GetAxis("Vertical")>.1f)
+        {
+            _rigidbody.AddForce(vector * Time.deltaTime);
+        }
+        // zorgen dat het per seconde gaat idpv per frame
+        
+	}
+}
